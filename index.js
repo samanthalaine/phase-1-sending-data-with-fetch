97992928ffcdc2545393e7fb888625da6a3c4918
Samantha Laine <samanthalaine8@gmail.com>
2021-08-02 00:28:46 -0400

    commit tree 715609554f68de548534b4ff17c0ae02a91fa396

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Add your code here
+
+//makes a POST request to /users with a name and email
+let body = document.querySelector("body")
+
+function submitData(userName, userEmail){
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers:{ 
+            "Content-Type": "application/json",
+            Accept: "application/json",
+
+        },
+        body: JSON.stringify({
+            name: userName,
+            email: userEmail,
+          }),
+    })
+
+//handles the POST request response, retrieves the new id value and appends it to the DOM
+        .then(function (response){
+            return response.json()
+        })
+        .then(function (object){
+            body.append(object.id)
+        })
+
+//handles a failed POST request using catch, appends the error message to the DOM        
+        .catch(function(error){
+            alert("oh no!")
+            body.append(error.message)
+        })
+
+}
